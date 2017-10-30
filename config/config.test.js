6b5b8d0af0053711b2801298d0a31a3ea2fcc6e4
gatinul <13252716435@163.com>
2017-10-30 09:57:34 +0800

    change:config

---
+++ config/config.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.mysql = {
+  // 单数据库信息配置
+  client: {
+    host: '59.110.233.80',
+    port: '3306',
+    user: 'root',
+    password: 'cool0830~',
+    database: 'Koa',
+  },
+  // 是否加载到 app 上，默认开启
+  app: true,
+  // 是否加载到 agent 上，默认关闭
+  agent: false,
+};
