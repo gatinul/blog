7d573128f15400aa219e7245e3bfd42b1dc409be
gatinul <13252716435@163.com>
2017-10-30 14:55:35 +0800

    chore:change path

--- test/index.test.js
+++ test/index.test.js
@@ -7,7 +7,7 @@ describe('test/app/controller/home.test.js', () => {
 
 
   it('should assert', function* () {
-    const pkg = require('../../../package.json');
+    const pkg = require('../package.json');
     assert(app.config.keys.startsWith(pkg.name));
 
     // const ctx = app.mockContext({});
