df3bfe39b23a9f4d7ef116096a59ffc2fe1f83b5
gatinul <13252716435@163.com>
2017-11-14 23:15:07 +0800

    :sparkles: Elastic create

--- app/lib/elasticsearch.js
+++ app/lib/elasticsearch.js
@@ -6,10 +6,20 @@ const esClient = new elasticsearch.Client({
   host: '127.0.0.1:9200',
   log: 'error',
 });
-esClient.search({
-  q: 'pants',
-}).then(function(body) {
-  console.log(body);
-}, function(error) {
-  console.trace(error.message);
+
+
+esClient.create({
+  index: 'koa',
+  type: 'blog_tag_md',
+  id: '1',
+  body: {
+    tag_name: 'rebecca',
+    md: 'hello.md',
+    remark: '',
+    create_time: '2017-10-25 16:38:40',
+  },
+}, function(error, response) {
+  // ...
+  console.log(response);
 });
+
