a1c2de033d75bd7c06fea4b6999e80feff04c2d7
gatinul <13252716435@163.com>
2018-05-11 20:50:14 +0800

    小程序能人

--- app/router.js
+++ app/router.js
@@ -10,4 +10,14 @@ module.exports = app => {
   app.post('/mini/middleApi', 'mini.middleApi');
   app.get('/mini/getAccessToken', 'mini.getAccessToken');
   app.post('/mini/getSessionKey', 'mini.getSessionKey');
+  // 能人
+  app.post('/mini/ableQueryByLoginName', 'mini.ableQueryByLoginName');
+  app.post('/mini/ableQrcBind', 'mini.ableQrcBind');
+  app.post('/mini/ableQuerySecond', 'mini.ableQuerySecond');
+  app.post('/mini/ableQueryReviewing', 'mini.ableQueryReviewing');
+  app.post('/mini/ableRejectSecond', 'mini.ableRejectSecond');
+  app.post('/mini/ableQueryOrder', 'mini.ableQueryOrder');
+  app.post('/mini/ableGetAQrcByAbleId', 'mini.ableGetAQrcByAbleId');
+  app.post('/mini/ableQueryQrcByBAbleId', 'mini.ableQueryQrcByBAbleId');
+  app.post('/mini/retryBindABQrc', 'mini.retryBindABQrc');
 };
